837823b320e06031aa96696cc25bb36c8df7d513
James Sutherland <jrsutherland78@googlemail.com>
2019-05-09 23:16:12 +0100

    add .App #getWord

--- src/main/java/guess/App.java
+++ src/main/java/guess/App.java
@@ -1,9 +1,32 @@
 package guess;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App {
 
+  private String url = "http://www.randomword.com";
+  private String randomWord = "";
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+  public App() {}
+
+  public String getWord() {
+    Elements word = getDocumentHelper().select("#random_word");
+    randomWord = word.text();
+    System.out.println(randomWord);
+    return randomWord;
+  }
+
+  public Document getDocumentHelper() {
+    Document doc = null;
+    try {
+      doc = Jsoup.connect(url).get();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return doc;
+  }
 }
--- src/test/java/guess/AppTest.java
+++ src/test/java/guess/AppTest.java
@@ -1,11 +1,25 @@
 package guess;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+    @Test public void testgetWordReturnsString() {
+      Document document = Jsoup.parse("<html><head></head><body><div id='random_word'>test</div></body><html>");
+      App appMock = spy(App.class);
+      when(appMock.getDocumentHelper()).thenReturn(document);
+
+      assertEquals("test", appMock.getWord());
     }
 }
